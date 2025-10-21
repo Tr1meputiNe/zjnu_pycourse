e083e4cdd18006e35866b76df78816e648341700
Tr1meputiNe <l3502461169@icloud.com>
2025-10-21 12:36:30 +0800

    hmwk

--- homework/draft.py
+++
@@ -1,7 +0,0 @@
-#Q1
-a = input()
-b = input()
-c = input()
-
-print(a, b, c)
-print(a, b, c, sep = "-")
---
+++ homework/lec2_hmwk_draft.py
@@ -0,0 +1,48 @@
+#Q1
+'''
+a = input()
+b = input()
+c = input()
+
+print(a, b, c)
+print(a, b, c, sep = "-")
+'''
+
+#Q2
+'''
+a = input()
+b = input()
+c = input()
+d = input()
+
+print("今天是{}年{}月{}日，天气{}。".format(a, b, c, d))
+'''
+
+#Q3
+'''
+a = input()
+b = input()
+c = input()
+d = input()
+print(f"今天是{a}年{b}月{c}日，天气{d}。")
+'''
+
+#Q4
+'''
+n = int(input())
+X = n * 50
+Y = n * 2
+Z = n * 100
+
+print(f" 为了准备{n}人份的面条，你需要{X:.1f}毫升水，{Y:.1f}克盐，{Z:.1f}克面粉。")
+'''
+
+#Q5
+'''
+f = float(input())
+c = 5 * (f - 32) / 9
+
+print(f"{c:.2f}")
+'''
+
+#Q6
