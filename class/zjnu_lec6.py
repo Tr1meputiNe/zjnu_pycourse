00f06cc9a72e597db0c5ddcf1dacf1bc14230d1a
Tr1meputiNe <l3502461169@icloud.com>
2025-11-19 08:10:05 +0800

    class_commits_unfinish ver.

--- class/zjnu_lec6.py
+++ class/zjnu_lec6.py
@@ -1,6 +1,6 @@
 #水仙花数
 n1 = int(input())
-for x in range (10**(n1 - 1), 10**n):
+for x in range (10**(n1 - 1), 10**n1):
     l = map(int, str(x))
     s = 0                           
 #for i in l:     s += i**n
@@ -15,5 +15,36 @@ for n2 in range(2, 10000):
             break
     else:
         print(n2)
+
+#哥德巴赫猜想
+n = int(input())
+for n in range(4, 10001, 2):
+    for a in range(2, n // 2 + 1):
+        b = n - a 
+        #判断a和b是否都为素数
+        for i in range(2, int(a)**0.5 + 1):
+            if a % i ==0:
+                break
+        else:
+            for i in range(2, int(b)**0.5 + 1):
+                if b % i == 0:
+                    break
+            else:
+                print(n, a, b)
+
+
+#百钱买百鸡
+for x in range(1, 21):
+    for y in range(1, 34):
+        z = 100 - x - y
+        if z % 3 == 0 and 5*x + 3*y + z == 100:
+            print(x, y, z)
+
+#牛顿迭代法
+
+
+    
+
+
     
 
